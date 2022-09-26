15a764c164520e298dbe5154121baf0f0dce942f
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-09-26 11:26:21 +0200

    export components

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export {default} from './Card';
+export {default as CardContent} from './CardContent';
+export { default as CardContentTop } from "./CardContentTop";
+export {default as CardHeader} from './CardHeader';
+export {default as CardImage} from './CardImage';
+export {default as Button } from './Button';
