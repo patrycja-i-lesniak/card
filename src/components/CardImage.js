07a7b269838dccf6976f22af350a0c2b7b9f1e83
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-09-26 11:25:30 +0200

    add CardImage components

---
+++ src/components/CardImage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import image from 'assets/sequoia.jpeg';
+
+export default function CardImage() {
+  return (
+    <div>
+      <img src={image} alt="sequoia" />
+    </div>
+  );
+}
